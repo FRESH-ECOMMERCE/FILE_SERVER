d6d78a77d3921c74cb43a2a4a4fc1443dc7364ce
misdarmanto <misdarmanto1922@gmail.com>
2025-11-13 21:55:30 +0700

    fix max limit

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ require('./workers/zipExtractWorker');
 const app = express();
 const PORT = appConfigs.port;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 app.use(
     cors({
